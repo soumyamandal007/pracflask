be5704ce6fb8ad281af556cd4e30822e21a663de
soumyamandal007 <soumya.acroy@gmail.com>
2022-10-07 01:43:18 -0700

    first commit

---
+++ website/auth.py
@@ -0,0 +1,32 @@
+from flask import Blueprint,render_template, request, flash
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+@auth.route('/logout')
+def logout():
+    return '<p>logout</p>'
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 4', category='error')
+        elif len(firstName) < 2:
+            flash('First Name must be greater than 2', category='error')
+        elif (password1 != password2):
+            flash('Password must be same', category='error')
+        elif len(password1) < 7:
+            flash('Password must be greater than 7', category='error')
+        else:
+            #add user to the database
+            flash('Account created', category='success')
+    return render_template('sign_up.html')
